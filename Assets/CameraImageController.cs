ac7381bc1635688f6c3de2a92f767c4bf4014500
ShuSorimachi <warptown6741@gmail.com>
2020-07-22 14:52:54 +0900

    first comit

---
+++ Assets/CameraImageController.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System;
+using Unity.Collections.LowLevel.Unsafe;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class CameraImageController : MonoBehaviour
+{
+    public ARCameraManager cameraManager;
+
+    Texture2D mTexture;
+    private MeshRenderer mRenderer;
+
+    private void Start()
+    {
+        mRenderer = GetComponent<MeshRenderer>();
+    }
+
+    private void OnEnable()
+    {
+        cameraManager.frameReceived += OnCameraFrameReceied;
+    }
+
+    private void OnDisable()
+    {
+        cameraManager.frameReceived -= OnCameraFrameReceied;
+    }
+
+    unsafe void OnCameraFrameReceied(ARCameraFrameEventArgs eventArgs){
+        XRCpuImage image;
+        if (!cameraManager.TryGetLatestImage(out image))
+            return;
+
+        var conversionParams = new XRCpuImage.ConversionParams
+            (
+             image,
+             TextureFormat.RGBA32,
+             XRCpuImage.Transformation.None
+            );
+
+        if(mTexture == null || mTexture.width != image.width || mTexture.height != image.height)
+        {
+            mTexture = new Texture2D(
+                conversionParams.outputDimensions.x,
+                conversionParams.outputDimensions.y,
+                conversionParams.outputFormat,
+                false);
+        }
+
+        var buffer = mTexture.GetRawTextureData<byte>();
+        image.Convert(conversionParams, new IntPtr(buffer.GetUnsafePtr()), buffer.Length);
+
+        mTexture.Apply();
+        mRenderer.material.mainTexture = mTexture;
+
+        buffer.Dispose();
+        image.Dispose();
+
+    }
+}
